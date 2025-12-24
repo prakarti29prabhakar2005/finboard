b66a8c7a081a25cbb34f73701b5936197e20d94d
Prakarti Prabhakar <prakarti@nandico.in>
2025-12-24 23:11:08 +0530

    Initial FinBoard setup

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,9 @@
+export default function Dashboard() {
+  return (
+    <div className="grid grid-cols-3 gap-4 p-6">
+      <div className="bg-white p-4 shadow rounded">Widget 1</div>
+      <div className="bg-white p-4 shadow rounded">Widget 2</div>
+      <div className="bg-white p-4 shadow rounded">Widget 3</div>
+    </div>
+  );
+}
